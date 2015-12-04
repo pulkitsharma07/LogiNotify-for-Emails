b5ea8725dc42ffdd4f9ac57fa4d1e56ff34f71c0
Pulkit Sharma <avastpulkit@gmail.com>
2015-12-04 11:56:58 +0530

    Changed the Atom feed to the default one(removed personal label)

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ def count_unread_msgs(user, passwd):
 
     opener = urllib2.build_opener(auth_handler)
     urllib2.install_opener(opener)
-    feed = urllib2.urlopen('https://mail.google.com/mail/feed/atom/%5Esmartlabel_personal')
+    feed = urllib2.urlopen('https://mail.google.com/mail/feed/atom/')
     parsed = parse(feed.read())
     return parsed.feed.fullcount
 
