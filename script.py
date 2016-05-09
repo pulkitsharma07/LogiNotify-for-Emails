274d646ae68db66ad3e2fc3730b744271e96afad
Pulkit Sharma <avastpulkit@gmail.com>
2016-05-09 15:33:44 +0530

    OOPified

--- script.py
+++ script.py
@@ -3,44 +3,61 @@ from time import sleep
 from feedparser import parse
 import urllib2
 
-def count_unread_msgs(user, passwd):
-    auth_handler = urllib2.HTTPBasicAuthHandler()
+class Blinker:
+    
+    def __init__(self,username,password,R,G,B):
+        self.username = username
+        self.password = password
+        self.R = int(R > 0)
+        self.G = int(G > 0)
+        self.B = int(B > 0)
 
-    auth_handler.add_password(
-    realm='mail.google.com',
-    uri='https://mail.google.com',
-    user='%s@gmail.com' % user,
-    passwd= passwd
-    )
 
-    opener = urllib2.build_opener(auth_handler)
-    urllib2.install_opener(opener)
-    try:
-        feed = urllib2.urlopen('https://mail.google.com/mail/feed/atom/')
-    except:
-        print "Cannot connect to Atom feed"
-        return 0
-        
-    parsed = parse(feed.read())
-    return parsed.feed.fullcount
+    def count_unread_msgs(self):
+        auth_handler = urllib2.HTTPBasicAuthHandler()
+
+        auth_handler.add_password(
+        realm='mail.google.com',
+        uri='https://mail.google.com',
+        user='%s@gmail.com' % self.username,
+        passwd= self.password
+        )
+
+        opener = urllib2.build_opener(auth_handler)
+        urllib2.install_opener(opener)
+        try:
+            feed = urllib2.urlopen('https://mail.google.com/mail/feed/atom/')
+        except:
+            print "Cannot connect to Atom feed"
+            return 0
+            
+        parsed = parse(feed.read())
+        return parsed.feed.fullcount
+
+
+    def blink(self):
+        count = self.count_unread_msgs()
+        print count
+        if int(count) > 0:
+             logi_led.logi_led_flash_lighting(self.R,self.G,self.B,800*int(count),100)
+             sleep(int(count))
+
+        logi_led.logi_led_set_lighting(0,100,0)
+
 
 
 logi_led.logi_led_init()
 logi_led.logi_led_save_current_lighting()
 
+Accounts = []
+
+Accounts.append(Blinker('username_account1','password_account1',0,1,1))
+Accounts.append(Blinker('username_account2','password_account2',1,1,0))
+
 while True:
-
-    count = count_unread_msgs('username_account1','password_account1')   
-    if int(count) > 0:
-         logi_led.logi_led_flash_lighting(0,100,100,800*int(count),100)
-         sleep(int(count))
-
-    logi_led.logi_led_set_lighting(0,100,0)
-
-    count = count_unread_msgs('username_account2','password_account2')
-    if int(count) > 0:
-        logi_led.logi_led_flash_lighting(100,100,0,800*int(count),100)
-        sleep(int(count))
-
-    logi_led.logi_led_set_lighting(0,100,0)
+    for act in Accounts:
+        act.blink()
     sleep(5)
+
+
+
